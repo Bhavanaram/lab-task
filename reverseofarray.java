53dfa32429932ac2d4e1e86c44c0b2ae1837d0c7
Bhavana Rs <107992570+Bhavanaram@users.noreply.github.com>
2022-07-15 21:22:01 +0530

    Add files via upload

--- reverseofarray.java
+++ reverseofarray.java
@@ -2,7 +2,7 @@ class ReverseOfArray{
 
 public static void main(String args[])
 {
-	int a[]={1,2,3,4,5}; //initialize the array elements
+	int a[]={10,9,8,7,6,5}; //initialize the array elements
 	int len=a.length; // initialize the length of array
 	for(int i=a.length-1;i>=0;i--) // loop for array values
 	{
